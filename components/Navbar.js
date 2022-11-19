26ce467f64f0263c7232a5e622fca5bbbf8e13f1
YTW7 <myahya96@gmail.com>
2022-11-19 15:54:36 +0530

    Yo man we did it

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import { truncate } from '../utils/string'
+import Script from 'next/script'
+import Head from 'next/head'
 
 
 require('@solana/wallet-adapter-react-ui/styles.css')
@@ -8,9 +10,28 @@ require('@solana/wallet-adapter-react-ui/styles.css')
 
 const Navbar = ({ connected, publicKey}) => {
   return (
+    <>
+    <Head>
+    <script>
+  // It's best to inline this in `head` to avoid FOUC (flash of unstyled content) when changing pages or themes
+  if (
+    localStorage.getItem('color-theme') === 'dark' ||
+    (!('color-theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+</script>
+    </Head>
+   <Script src="../path/to/flowbite/dist/flowbite.bundle.js" />
+
     <div>
       <header class="text-gray-600 body-font">
+        
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    
     <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
       <a class="mr-5 hover:text-gray-900">Home</a>
       <a class="mr-5 hover:text-gray-900">About</a>
@@ -32,14 +53,47 @@ const Navbar = ({ connected, publicKey}) => {
           <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
       </button> */}
+      <button
+  id="theme-toggle"
+  type="button"
+  class="text-red-500 dark:text-gray-400 hover:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-xl p-2.5"
+>
+  <svg
+    id="theme-toggle-dark-icon"
+    class="w-5 h-5 hidden"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+    ></path>
+  </svg>
+  <svg
+    id="theme-toggle-light-icon"
+    class="w-5 h-5 "
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+    ></path>
+  </svg>
+</button>
        <WalletMultiButton className="phantom-button" >
        <img class="w-8 h-8 mr-2 rounded-full bg-red-500" src="https://cdn.icon-icons.com/icons2/2645/PNG/512/person_circle_icon_159926.png" alt=""/>
                     <span className="text-sm font-black text-red-600">{connected ? truncate(publicKey.toString()) : 'Connect Wallet'}</span>
                 </WalletMultiButton>
+                
+                
     </div>
   </div>
 </header>
     </div>
+    </>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import dynamic from 'next/dynamic'
+import Navbar from '../components/Navbar'
+
 const WalletConnectionProvider = dynamic(() => import('../context/WalletConnectionProvider'), {
   ssr: false,
 })
